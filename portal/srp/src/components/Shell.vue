86a4b93a269bf440a18828a71f49eaf81695ffaf
anu.viswan@gmail.com <njunni@1320>
2020-12-18 21:33:27 +0530

    adding files for srp

---
+++ portal/srp/src/components/Shell.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row">
+    <div class="col-3">
+      Rule here
+    </div>
+    <div class="col-9">
+      Description here
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Shell",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ portal/srp/src/components/generic/RuleList.vue
@@ -0,0 +1,9 @@
+<template> </template>
+
+<script>
+export default {
+  name: "RuleList",
+};
+</script>
+
+<style></style>
