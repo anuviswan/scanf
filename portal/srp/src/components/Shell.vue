1b62ef148e7ff6b9a949da7e62a3ca6ea28f0d73
anu.viswan@gmail.com <njunni@1320>
2020-12-20 05:10:23 +0530

    added support for vue showdown

--- portal/srp/src/components/Shell.vue
+++ portal/srp/src/components/Shell.vue
@@ -2,17 +2,21 @@
   <v-container>
     <v-row>
       <v-col cols="3" align-self="start"><RuleList /></v-col>
-      <v-col cols="9"><v-card>Rule Description</v-card></v-col>
+      <v-col cols="9"
+        ><v-card><RuleDescription /></v-card
+      ></v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
 import RuleList from "./rules/RuleList";
+import RuleDescription from "./rules/RuleDescription";
 export default {
   name: "Shell",
   components: {
     RuleList,
+    RuleDescription,
   },
   props: {
     msg: String,
---
+++ portal/srp/src/components/rules/RuleDescription.vue
@@ -0,0 +1,23 @@
+<template>
+  <VueShowdown
+    v-bind:markdown="this.description"
+    flavor="github"
+    :options="{ emoji: true }"
+  />
+</template>
+
+<script>
+import { VueShowdown } from "vue-showdown";
+
+export default {
+  name: "RuleDescription",
+  components: { VueShowdown },
+  data() {
+    return {
+      description: "## Avoid using async void",
+    };
+  },
+};
+</script>
+
+<style></style>
