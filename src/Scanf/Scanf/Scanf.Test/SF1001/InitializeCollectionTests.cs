f4ef2890e69b73830d4128462997a59df57aff09
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-03-21 05:05:07 +0530

    adding unit tests

---
+++ src/Scanf/Scanf/Scanf.Test/SF1001/InitializeCollectionTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scanf.CodeSmell;
+using VerifyCS = Scanf.Test.CSharpCodeFixVerifier<
+Scanf.CodeSmell.AsyncVoidAnalyzer,
+Scanf.CodeSmell.AsyncVoidCodeFixProvider>;
+
+
+namespace Scanf.Test.SF1001
+{
+    public class SF1001Tests
+    {
+        [TestMethod]
+        [DynamicData(nameof(GetInvalidData), DynamicDataSourceType.Method)]
+        public async Task CodeThatRequireFix(string inputFile, int line, int col, string value, string expectedCodeFixFile)
+        {
+            var inputSource = File.ReadAllText(inputFile);
+            var expectedCodeFixSource = File.ReadAllText(expectedCodeFixFile);
+            var rule = VerifyCS.Diagnostic(InitializeCollectionAnalyzer.DiagnosticId);
+            var expected = rule.WithLocation(line, col).WithArguments(value.Trim());
+            await VerifyCS.VerifyCodeFixAsync(inputSource, expected, expectedCodeFixSource);
+        }
+
+        private static IEnumerable<object[]> GetInvalidData()
+        {
+            yield return new object[]
+            {
+                @"SF1001\TestData\Diagnostics\CollectionWithSizeInitialized.cs",
+            };
+
+            
+        }
+    }
+}
---
+++ src/Scanf/Scanf/Scanf.Test/SF1001/TestData/Diagnostics/CollectionWithSizeInitialized.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scanf.Test.SF1001.TestData.Diagnostics
+{
+    public class CollectionWithSizeInitialized
+    {
+        public void SampleMethod()
+        {
+            var collection = new List<string>();
+        }
+    }
+}
