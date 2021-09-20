3a3c5b5f5bcf9b79aa30154072ee016e28c7d2f6
Anu Viswan <anu.viswan@gmail.com>
2021-09-21 04:24:16 +0530

    Working on Test Scenario

---
+++ src/TestSolution/TestSolution.Console/SF1011.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestSolution.Console
+{
+    [Flags]
+    public enum EnumWithFlagAttribute
+    {
+        None,
+        First,
+        Second,
+        Third,
+        Four
+    }
+}
